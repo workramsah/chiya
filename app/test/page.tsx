fb8e278c2cb6212414e8eab6afda3b003e11d3fb
workramsah <workramsah@gmail.com>
2026-01-07 14:22:39 +0545

    fix error of test/page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,22 +1,7 @@
-// tailwind.config.js
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        'infinite-scroll-right': 'scrollRight 20s linear infinite',
-        'infinite-scroll-left': 'scrollLeft 20s linear infinite',
-      },
-      keyframes: {
-        scrollRight: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
-        scrollLeft: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
-    },
-  },
-  plugins: [],
+export default function Page(){
+  return(
+    <>
+    <div>hello world from test page</div>
+    </>
+  )
 }
