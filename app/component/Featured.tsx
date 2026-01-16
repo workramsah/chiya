1ed7ac4bd8f4e1b395b2304e4a6fd258eb060c3e
workramsah <workramsah@gmail.com>
2026-01-16 12:31:07 +0545

    fix all pages

--- app/component/Featured.tsx
+++ app/component/Featured.tsx
@@ -7,13 +7,13 @@ export default function Featured(){
         <div className="flex justify-center "><span className="font-bold text-2xl">Featured</span> <span className="font-bold text-2xl text-amber-400">Products</span></div>
         
         <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 space-x-3">
-            <Cup age={3} price={33}  name="tea"/>
-            <Cup age={3} name="tea"/>
-            <Cup age={3} name="tea"/>
-            <Cup age={3} name="tea"/>
-            <Cup age={3} name="tea"/>
-            <Cup age={3} name="tea"/>
-            <Cup age={3} name="tea"/>
+            <Cup  price={33}  name="tea"/>
+            <Cup  price={33}  name="tea"/>
+            <Cup  price={33}  name="tea"/>
+            
+            <Cup  price={33}  name="tea"/>
+            <Cup  price={33}  name="tea"/>
+            <Cup  price={33}  name="tea"/>
         </div>
         </div>
         </>
