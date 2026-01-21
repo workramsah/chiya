d9e2ba8fad2c9ec3eb6896fe1e8a86ad69edfacb
workramsah <workramsah@gmail.com>
2026-01-21 16:09:21 +0545

    fix authencation problem in vercel

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,17 @@
 import { createAuthClient } from "better-auth/react"
+const getBaseURL = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  return "http://localhost:3000";
+};
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: getBaseURL(),
 });
-export const { signIn, signUp, useSession } = createAuthClient()
+
+export const { signIn, signUp, useSession } = authClient;
